a60f56d21ccb70373eeaf85ba3f5c2c7a80b25db
LBenzahia <lakhdar.benzahia@gmail.com>
2018-09-21 18:53:15 +0100

    made mongoose pagination support mongoose 5

--- index.js
+++ index.js
@@ -16,75 +16,104 @@
  * @returns {Promise}
  */
 
-function paginate(query, options, callback) {
-  query = query || {};
-  options = Object.assign({}, paginate.options, options);
-  let select = options.select;
-  let sort = options.sort;
-  let populate = options.populate;
-  let lean = options.lean || false;
-  let leanWithId = options.leanWithId ? options.leanWithId : true;
-  let limit = options.limit ? options.limit : 10;
-  let page, offset, skip, promises;
-  if (options.offset) {
-    offset = options.offset;
-    skip = offset;
-  } else if (options.page) {
-    page = options.page;
-    skip = (page - 1) * limit;
-  } else {
-    page = 1;
-    offset = 0;
-    skip = offset;
-  }
-  if (limit) {
-    let docsQuery = this.find(query)
-      .select(select)
-      .sort(sort)
-      .skip(skip)
-      .limit(limit)
-      .lean(lean);
-    if (populate) {
-      [].concat(populate).forEach((item) => {
-        docsQuery.populate(item);
-      });
-    }
-    promises = {
-      docs: docsQuery.exec(),
-      count: this.count(query).exec()
-    };
-    if (lean && leanWithId) {
-      promises.docs = promises.docs.then((docs) => {
-        docs.forEach((doc) => {
-          doc.id = String(doc._id);
-        });
-        return docs;
-      });
-    }
-  }
-  promises = Object.keys(promises).map((x) => promises[x]);
-  return Promise.all(promises).then((data) => {
-    let result = {
-      docs: data.docs,
-      total: data.count,
-      limit: limit
-    };
-    if (offset !== undefined) {
-      result.offset = offset;
-    }
-    if (page !== undefined) {
-      result.page = page;
-      result.pages = Math.ceil(data.count / limit) || 1;
-    }
-    if (typeof callback === 'function') {
-      return callback(null, result);
-    }
-    let promise = new Promise();
-    promise.resolve(result);
-    return promise;
-  });
+// function paginate(query, options, callback) {
+//   query = query || {};
+//   options = Object.assign({}, paginate.options, options);
+//   let select = options.select;
+//   let sort = options.sort;
+//   let populate = options.populate;
+//   let lean = options.lean || false;
+//   let leanWithId = options.leanWithId ? options.leanWithId : true;
+//   let limit = options.limit ? options.limit : 10;
+//   let page, offset, skip, promises;
+//   if (options.offset) {
+//     offset = options.offset;
+//     skip = offset;
+//   } else if (options.page) {
+//     page = options.page;
+//     skip = (page - 1) * limit;
+//   } else {
+//     page = 1;
+//     offset = 0;
+//     skip = offset;
+//   }
+//   if (limit) {
+//     let docsQuery = this.find(query)
+//       .select(select)
+//       .sort(sort)
+//       .skip(skip)
+//       .limit(limit)
+//       .lean(lean);
+//     if (populate) {
+//       [].concat(populate).forEach((item) => {
+//         docsQuery.populate(item);
+//       });
+//     }
+//     promises = {
+//       docs: docsQuery.exec(),
+//       count: this.countDocuments(query).exec()
+//     };
+//     if (lean && leanWithId) {
+//       promises.docs = promises.docs.then((docs) => {
+//         docs.forEach((doc) => {
+//           doc.id = String(doc._id);
+//         });
+//         return docs;
+//       });
+//     }
+//   }
+//   promises = Object.keys(promises).map((x) => promises[x]);
+//   return Promise.all(promises).then((data) => {
+//     let result = {
+//       docs: data.docs,
+//       total: data.count,
+//       limit: limit
+//     };
+//     if (offset !== undefined) {
+//       result.offset = offset;
+//     }
+//     if (page !== undefined) {
+//       result.page = page;
+//       result.pages = Math.ceil(data.count / limit) || 1;
+//     }
+//     if (typeof callback === 'function') {
+//       return callback(null, result);
+//     }
+//     let promise = new Promise();
+//     promise.resolve(result);
+//     return promise;
+//   });
+// }
+
+function paginate(query, options) {
+    query   = query || {};
+    options = Object.assign({}, paginate.options, options);
+
+    const sort  = options.sort;
+    const limit = options.hasOwnProperty('limit') ? options.limit : 10;
+    const page  = options.page || 1;
+    const skip  = options.hasOwnProperty('page') ? ((page - 1) * limit) : 0;
+    const docs = limit
+     ? this.find(query).sort(sort).skip(skip).limit(limit).exec()
+     : query.exec();
+    const countDocuments = this.countDocuments(query).exec();
+
+    return Promise.all([docs, countDocuments]).then(function(values) {
+      return Promise.resolve({
+        docs:  values[0],
+        total: values[1],
+        limit: limit,
+        page: page,
+        pages: Math.ceil(values[1] / limit) || 1
+      })
+    });
 }
 
+module.exports = function(schema) {
+    schema.statics.paginate = paginate;
+};
+
+module.exports.paginate = paginate;
 /**
  * @param {Schema} schema
  */
